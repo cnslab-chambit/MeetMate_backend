69d74f240e8eb9647f789c06f760b29079b6f869
shin5774 <babo5774@naver.com>
2023-03-03 00:23:29 +0900

    feat: 백엔드 베이스 환경 구축

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MeetMate-backend
 2023-02 참빛설계학기 프로젝트 MeetMate 팀 backend 파트
+
+- 담당 팀원 \
+    신권일 
---
+++ meetmate/src/test/java/com/chambit/meetmate/MeetmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chambit.meetmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
